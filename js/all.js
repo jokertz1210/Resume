f4d996c14500ace90fb4c57ace1b3668cd840c86
Joker <jokertz1210@gmail.com>
2018-09-17 13:18:26 +0800

    Resume-1.0

--- js/all.js
+++ js/all.js
@@ -1,11 +1,11 @@
 $(window).scroll(function() {
   var scrollHeight = $(window).scrollTop();//滚动的高度
   if (scrollHeight > 50) {
-    $('.p1').css('width','90%')
-    $('.p2').css('width','90%')
-    $('.p3').css('width','60%')
-    $('.p4').css('width','75%')
-    $('.p5').css('width','80%')
+    $('.p1').css('width','95%')
+    $('.p2').css('width','80%')
+    $('.p3').css('width','90%')
+    $('.p4').css('width','70%')
+    $('.p5').css('width','60%')
   }
 })
 
